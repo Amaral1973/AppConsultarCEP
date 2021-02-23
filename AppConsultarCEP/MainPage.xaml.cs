8925e9e0c4ba5c85cdf6d4ee3b5da3c74c127f49
Amaral1973 <emersonb6@gmail.com>
2021-02-23 16:56:26 -0300

    Adicionar arquivos de projeto.

---
+++ AppConsultarCEP/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using AppConsultarCEP.Servico.Modelo;
+using AppConsultarCEP.Servico;
+
+namespace AppConsultarCEP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BOTAO.Clicked += BuscarCEP;
+        }
+
+        private void BuscarCEP(object sender, EventArgs args)
+        {
+            //Lógica do Programa
+            string cep = CEP.Text.Trim();
+            if (isValidCEP(cep))
+            {
+                try
+                {
+                    Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+                    if (end != null)
+                    {
+                        RESULTADO.Text = string.Format("Endereço: {2} de {3} {0},{1}", end.localidade, end.uf, end.logradouro, end.bairro);
+                    }
+                    else
+                    {
+                        DisplayAlert("Erro", "Endereço não encontrado para este CEP:"+ cep, "OK");
+                    }
+                }
+                catch(Exception e)
+                {
+                    DisplayAlert("Erro Crítico", e.Message, "OK");
+                }
+            }
+        }
+
+        private bool isValidCEP(string cep)
+        {
+            Boolean valido = true;
+            if(cep.Length != 8)
+            {
+                DisplayAlert("Erro", "CEP Inválido! O CEP deve conter 8 caracteres.", "OK");
+                valido = false;
+            }
+            int NovoCEP = 0;
+            if(!int.TryParse(cep, out NovoCEP))
+            {
+                DisplayAlert("Erro", "CEP Inválido! O CEP deve conter apenas números.", "OK");
+                valido = false;
+            }
+            return valido;
+        }
+
+        private void BOTAONOVO_Clicked(object sender, EventArgs e)
+        {
+            CEP.Text = "";
+            CEP.Focus();
+            RESULTADO.Text = "";
+        }
+    }
+}
